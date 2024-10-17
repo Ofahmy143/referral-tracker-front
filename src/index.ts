e57939b8bd41ec73e9cb2b512e7ccf6cc0b18510
Omar <102324541+Ofahmy143@users.noreply.github.com>
2024-10-17 14:24:03 +0300

    feat: add inviter id as args and remove ip from front
    Co-authored-by: Ghaith Kdimati <gaoia123@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
 export const handleRefferal = async (
+  inviterId: string,
   backendUrl: string,
   iosRedirectUrl: string,
   androidRedirectUrl: string
 ): Promise<void> => {
-  const params = new URLSearchParams(window.location.search);
-  const inviterId = params.get("inviter");
-
   if (inviterId) {
-    async function retrievePublicIp(): Promise<string> {
-      const response = await fetch("https://api.ipify.org?format=json");
-      const data = await response.json();
-      return data.ip;
-    }
 
     // Collect device info
     const screenWidth = window.screen.width;
     const screenHeight = window.screen.height;
-    const publicIP = await retrievePublicIp();
-
-    console.log("Inviter ID:", inviterId);
 
     // Send data to the backend
     await fetch(`${backendUrl}/store-referral`, {
@@ -27,7 +17,6 @@ export const handleRefferal = async (
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        publicIP,
         screenHeight,
         screenWidth,
         userAgent: window.navigator.userAgent,
